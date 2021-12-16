64cdf18e141d3181b3812f4aa06cfcedb65d278c
Louiso <gransullca.25@gmail.com>
2021-12-15 19:53:00 -0500

    [update]

--- src/App.tsx
+++ src/App.tsx
@@ -13,15 +13,12 @@ const App = function () {
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is:
-            {' '}
             {count}
           </button>
         </p>
         <p>
           Edit
-          {' '}
           <code>App.tsx</code>
-          {' '}
           and adwadwadawdaw to test HMR updates.
         </p>
         <p>
